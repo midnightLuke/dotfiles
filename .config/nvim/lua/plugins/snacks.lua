58e4c05d1bae77365e7becb623b8d4200082cdd5
Luke Bainbridge <luke.bainbridge@gmail.com>
2025-12-30 10:48:09 -0500

    Improvements, etc.

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,21 @@
+return {
+  "folke/snacks.nvim",
+  keys = {
+    -- Always open picker at project root
+    { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
+  },
+  opts = {
+    picker = {
+      hidden = true,
+      sources = {
+        -- These define their own options, we must override their defaults.
+        files = { hidden = true },
+        buffers = { hidden = true },
+        -- Explorer and the rest of the sources don't define their own opts
+        -- so it will use the picker options defined above and we can choose
+        -- to override them if desired.
+        explorer = { ignored = true },
+      },
+    },
+  },
+}
